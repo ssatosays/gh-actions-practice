a6de2e61570aa95be77a513ec2a148f7013a44e5
ssatosays <ssatosays@gmail.com>
2022-01-27 01:36:21 +0900

    Add few files

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
-
+"""System module."""
 def main():
-    a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    b = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    for i in a:
-        print(str([i * j for j in b]))
+    """main dayo"""
+    a_dayo = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    b_dayo = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for i in a_dayo:
+        print(str([i * j for j in b_dayo]))
 
 
 if __name__ == "__main__":
     main()
-
