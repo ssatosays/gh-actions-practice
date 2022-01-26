d770d35d6e335aa4141b3e331fa37c8ffe4388ac
ssatosays <ssatosays@gmail.com>
2022-01-27 01:26:27 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,4 +1,11 @@
-a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-for i in a:
-    print(str([i * j for j in b]))
+
+def main():
+    a = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    b = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for i in a:
+        print(str([i * j for j in b]))
+
+
+if __name__ == "__main__":
+    main()
+
