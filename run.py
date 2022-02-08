ede409929584a102fa9bf671d84c81742a7ea91d
ssatosays <ssatosays@gmail.com>
2022-02-08 23:15:07 +0900

    Update run.py

--- run.py
+++ run.py
@@ -1,10 +1,12 @@
-"""System module."""
+""" run.py
+"""
 def main():
-    """main dayo"""
-    a_dayo = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    b_dayo = [1, 2, 3, 4, 5, 6, 7, 8, 9]
-    for i in a_dayo:
-        print(str([i * j for j in b_dayo]))
+    """ main
+    """
+    a_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    b_list = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+    for i in a_list:
+        print(str([i * j for j in b_list]))
 
 
 if __name__ == "__main__":
